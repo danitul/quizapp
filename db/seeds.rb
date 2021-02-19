2fcb06106575e8212e34e2dc67703fa3f8a3b302
Danitul <dani.tulvan@gmail.com>
2021-02-19 01:50:07 +0100

    added seed file and README.md

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..5).each do |question_index|
+  answers = []
+  rand(2..5).times do |answer_index|
+    answers << Answer.new(name: "Answer #{answer_index}")
+  end
+  Question.create(name: "Question #{question_index}", answers: answers)
+end
